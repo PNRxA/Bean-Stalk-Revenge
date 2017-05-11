3109608286d813269bd6d2d2b1fa93ead17c5524
ADoublePlus <jmwonson@hotmail.com>
2017-05-11 15:10:22 +1000

    Began main menu and spawning scripts

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public Text waveCountdownText;
+
+    public float timeBetweenWaves = 5f;
+    public float countdown = 2f;
+
+    private int waveIndex = 0;
+
+	// Update is called once per frame
+	void Update ()
+    {
+		if(countdown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Floor(countdown).ToString();
+	}
+
+    IEnumerator SpawnWave()
+    {
+        //Debug.Log("Wave Incoming!");
+
+        for(int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        waveIndex++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
